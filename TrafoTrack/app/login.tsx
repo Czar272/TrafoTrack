09b2b9e6e07e5b47c4336ec648b09d01acd92031
Czar272 <pablo2003cesar@gmail.com>
2026-05-03 18:58:32 -0600

    feat: style login

--- TrafoTrack/app/login.tsx
+++ TrafoTrack/app/login.tsx
@@ -1,8 +1,15 @@
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { useEffect, useState } from "react";
 import { useRouter, Redirect } from "expo-router";
 import { useAuthStore } from "../store/authStore";
-import { Colors } from "@/constants/theme";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function Login() {
   const router = useRouter();
@@ -11,6 +18,9 @@ export default function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { colorPallet, styles } = getStyles();
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -25,6 +35,10 @@ export default function Login() {
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   if (isAuthenticated) {
     return <Redirect href={"/"} />;
   }
@@ -37,84 +51,121 @@ export default function Login() {
       </View>
 
       <View style={styles.formContainer}>
-        <View>
+        <View style={{ width: "70%" }}>
           <TextInput
-            placeholder="Email"
+            placeholder="Username / Email"
             value={email}
             onChangeText={setEmail}
             style={styles.textInput}
-            placeholderTextColor={Colors.dark.text}
+            placeholderTextColor={colorPallet.texto_secundario}
           />
 
-          <TextInput
-            placeholder="Password"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-            style={styles.textInput}
-            placeholderTextColor={Colors.dark.text}
-          />
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <Button
-            title="Login"
-            onPress={handleLogin}
-            color={Colors.light.text}
-          />
+          <View
+            style={{
+              justifyContent: "center",
+            }}
+          >
+            <TextInput
+              placeholder="Password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={!showPassword}
+              style={styles.textInput}
+              placeholderTextColor={colorPallet.texto_secundario}
+            />
+            <TouchableOpacity
+              onPress={togglePasswordVisibility}
+              style={styles.icon}
+            >
+              <MaterialIcons
+                name={showPassword ? "visibility" : "visibility-off"}
+                size={24}
+                color={colorPallet.texto_secundario}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button title="Login" onPress={handleLogin} color={"white"} />
+          </View>
         </View>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    alignContent: "center",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100%",
-    width: "100%",
-    padding: 20,
-    backgroundColor: Colors.dark.background,
-  },
-  textInput: {
-    borderWidth: 1,
-    marginBottom: 10,
-    padding: 10,
-    color: "black",
-    backgroundColor: Colors.dark.tabIconDefault,
-    borderRadius: 10,
-  },
-  title: {
-    color: Colors.dark.text,
-    fontSize: 30,
-    textAlign: "center",
-    fontFamily: "Audiowide_400Regular",
-  },
-  subTitle: {
-    color: Colors.dark.text,
-    fontSize: 22,
-    textAlign: "center",
-    alignSelf: "flex-end",
-    fontFamily: "Audiowide_400Regular",
-  },
-  titleContainer: {
-    height: "50%",
-    justifyContent: "center",
-    alignContent: "center",
-    alignItems: "center",
-  },
-  formContainer: {
-    height: "50%",
-    width: "100%",
-  },
-  buttonContainer: {
-    marginVertical: 50,
-    backgroundColor: Colors.light.tabIconSelected,
-    width: "30%",
-    alignSelf: "center",
-    borderRadius: 5,
-  },
-});
+const getStyles = () => {
+  const colorPallet = {
+    primario: "#1E3A8A",
+    secundario: "#3B82F6",
+    acento: "#06B6D4",
+    fondo: "#64748B",
+    texto_principal: "#0F172A",
+    texto_secundario: "#64748B",
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      display: "flex",
+      alignContent: "center",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100%",
+      width: "100%",
+      padding: 20,
+      backgroundColor: "#1517181e",
+    },
+    textInput: {
+      marginBottom: 10,
+      padding: 10,
+      color: colorPallet.texto_secundario,
+      borderBottomColor: "#284325",
+      borderBottomWidth: 2,
+      textAlign: "left",
+      alignItems: "flex-start",
+      width: "100%",
+      marginVertical: 20,
+    },
+    title: {
+      color: "#284325",
+      fontSize: 30,
+      textAlign: "center",
+      fontFamily: "Audiowide_400Regular",
+    },
+    subTitle: {
+      color: colorPallet.texto_secundario,
+      fontSize: 22,
+      textAlign: "center",
+      alignSelf: "flex-end",
+      fontFamily: "Audiowide_400Regular",
+    },
+    titleContainer: {
+      height: "30%",
+      justifyContent: "center",
+      alignContent: "center",
+      alignItems: "center",
+    },
+    formContainer: {
+      display: "flex",
+      width: "100%",
+      height: "35%",
+      justifyContent: "center",
+      alignContent: "center",
+      alignItems: "center",
+      marginBottom: 50,
+    },
+    buttonContainer: {
+      // backgroundColor: "#425c07",
+      backgroundColor: "#284325",
+      width: "100%",
+      alignSelf: "center",
+      marginTop: 100,
+    },
+    icon: {
+      aspectRatio: 1,
+      position: "absolute",
+      alignSelf: "flex-end",
+    },
+  });
+
+  return { colorPallet, styles };
+};
